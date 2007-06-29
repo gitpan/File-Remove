27e2373bb022cb6bb7e7a296a7c92de1bf433a85
Adam Kennedy <adamk@cpan.org>
2007-06-29 02:56:32 +0000

    Import of ADAMK/File-Remove-0.36 from CPAN.
    gitpan-cpan-distribution: File-Remove
    gitpan-cpan-version:      0.36
    gitpan-cpan-path:         ADAMK/File-Remove-0.36.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/File/Remove.pm
+++ lib/File/Remove.pm
@@ -3,9 +3,12 @@ package File::Remove;
 use strict;
 use vars qw(@EXPORT_OK @ISA $VERSION $debug $unlink $rmdir);
 BEGIN {
-	$VERSION   = '0.34';
+	$VERSION   = '0.36';
 	@ISA       = qw(Exporter);
 	@EXPORT_OK = qw(remove rm trash); # nothing by default :)
+
+	# Booleanise the debug flag
+	$debug = !! $debug;
 }
 
 # If we ever need a Mac::Glue object,
@@ -14,15 +17,23 @@ my $glue;
 
 use File::Spec ();
 use File::Path ();
-use File::Glob qw(bsd_glob);
+use File::Glob ();
 
 sub expand (@) {
 	map { File::Glob::bsd_glob($_) } @_;
 }
 
+# $debug variable must be set before loading File::Remove.
+# Convert to a constant to allow debugging code to be pruned out.
+use constant DEBUG => $debug;
+
 # Are we on VMS?
 # If so copy File::Path and assume VMS::Filespec is loaded
-use constant IS_VMS => $^O eq 'VMS';
+use constant IS_VMS   => $^O eq 'VMS';
+
+# Are we on Win32?
+# If so write permissions does not imply deletion permissions
+use constant IS_WIN32 => $^O eq 'MSWin32';
 
 
 
@@ -42,26 +53,56 @@ sub remove (@) {
 	my @removes;
 	foreach my $path ( @files ) {
 		unless ( -e $path ) {
-			print "missing: $path\n" if $debug;
+			print "missing: $path\n" if DEBUG;
 			push @removes, $path; # Say we deleted it
 			next;
 		}
-		unless ( IS_VMS ? VMS::Filespec::candelete($path) : -w $path ) {
-			print "nowrite: $path\n" if $debug;
+		my $can_delete;
+		if ( IS_VMS ) {
+			$can_delete = VMS::Filespec::candelete($path);
+		} elsif ( IS_WIN32 ) {
+			# Assume we can delete it for the moment
+			$can_delete = 1;
+		} elsif ( -w $path ) {
+			# We have write permissions already
+			$can_delete = 1;
+		} elsif ( $< == 0 ) {
+			# Unixy and root
+			$can_delete = 1;
+		} elsif ( (lstat($path))[4] == $< ) {
+			# I own the file
+			$can_delete = 1;
+		} else {
+			# I don't think we can delete it
+			$can_delete = 0;
+		}
+		unless ( $can_delete ) {
+			print "nowrite: $path\n" if DEBUG;
 			next;
 		}
 
 		if ( -f $path or -l $path ) {
-			print "file: $path\n" if $debug;
+			print "file: $path\n" if DEBUG;
+			unless ( -w $path ) {
+				# Make the file writable (implementation from File::Path)
+				(undef, undef, my $rp) = lstat $path or next;
+				$rp &= 07777; # Don't forget setuid, setgid, sticky bits
+				$rp |= 0600;  # Turn on user read/write
+				chmod $rp, $path;
+			}
 			if ( $unlink ? $unlink->($path) : unlink($path) ) {
+				# Failed to delete the file
+				next if -e $path;
 				push @removes, $path;
 			}
 
 		} elsif ( -d $path ) {
-			print "dir: $path\n" if $debug;
+			print "dir: $path\n" if DEBUG;
 			my $dir = File::Spec->canonpath( $path );
 			if ( $$recursive ) {
-				if ( File::Path::rmtree( [ $dir ], $debug, 0 ) ) {
+				if ( File::Path::rmtree( [ $dir ], DEBUG, 0 ) ) {
+					# Failed to delete the directory
+					next if -e $path;
 					push @removes, $path;
 				}
 
@@ -69,12 +110,14 @@ sub remove (@) {
 				my ($save_mode) = (stat $dir)[2];
 				chmod $save_mode & 0777, $dir; # just in case we cannot remove it.
 				if ( $rmdir ? $rmdir->($dir) : rmdir($dir) ) {
+					# Failed to delete the directory
+					next if -e $path;
 					push @removes, $path;
 				}
 			}
 
 		} else {
-			print "???: $path\n" if $debug;
+			print "???: $path\n" if DEBUG;
 		}
 	}
 
@@ -159,7 +202,7 @@ B<File::Remove::trash> accepts the same arguments as B<remove>, with
 the addition of an optional, infrequently used "other platforms"
 hashref.
 
-=head1 METHODS
+=head1 SUBROUTINES
 
 =over 4
 
